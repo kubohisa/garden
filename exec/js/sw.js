559d9db81e327a9e61e5974a1180e1ec4bb42843
kubohisa <kubohisa@gmail.com>
2024-08-30 18:24:55 +0900

    Update sw.js
    Fix.

--- exec/js/sw.js
+++ exec/js/sw.js
@@ -12,19 +12,17 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
 	e.waitUntil(
 		caches.
-		open(CACHE_NAME).
-		then(function(cache) {
+		open(CACHE_NAME).then(function(cache) {
 			return cache.addAll(filesToCache);
 		})
 	);
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', function(e) {
 	var cacheWhitelist = [CACHE_NAME];
 
-	event.waitUntil(
-		caches.keys().
-		then((cacheNames) => {
+	e.waitUntil(
+		caches.keys().then((cacheNames) => {
 			return Promise.all(
 				cacheNames.map((cacheName) => {
 					if (cacheWhitelist.indexOf(cacheName) === -1) {
@@ -38,9 +36,7 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', function(e) {
 	e.respondWith(
-		caches.
-		match(e.request).
-		then(function(response) {
+		caches.match(e.request).then(function(response) {
 			return response ? response : fetch(e.request);
 		})
 	);
